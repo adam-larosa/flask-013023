634e2bf9c39ef72a266d2043958000e6a4950898
Adam La Rosa <adam.larosa@flatironschool.com>
2023-04-05 05:15:32 -0700

    migrated

--- server/app.py
+++ server/app.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_migrate import Migrate
+
+from models import db
+
 
 app = Flask( __name__ )
+
+app.config[ 'SQLALCHEMY_DATABASE_URI' ] = 'sqlite:///meow.db' 
+app.config[ 'SQLALCHEMY_TRACK_MODIFICATIONS' ] = False
+
+migrate = Migrate( app, db )
+
+db.init_app( app )
+
+
+
+
 
 @app.route( '/' )
 def index():
