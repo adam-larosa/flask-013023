79c3d11ad72932857dec102efc674cc08aff7c41
Adam La Rosa <adam.larosa@flatironschool.com>
2023-04-05 12:29:58 -0700

    wed fin

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, make_response
+from flask import Flask, make_response, request
 from flask_migrate import Migrate
+import ipdb
 
 from models import db, Game
 
@@ -22,31 +23,49 @@ def index():
     return "<h1>hello meow!!</h1>"
 
 
-@app.route( '/games' )
+@app.route( '/games', methods = [ 'GET', 'POST' ] )
 def games():
-    games_list = []
-    for game in Game.query.all():
-        game_dict = {
-            "id": game.id,
-            "title": game.title,
-            "price": game.price
-        }
-        games_list.append( game_dict )
-    
-    response = make_response( games_list, 200  )
-    
-    return response
+
+    if request.method == 'GET':
+        games_list = []
+        for game in Game.query.all():
+            game_dict = {
+                "id": game.id,
+                "title": game.title,
+                "price": game.price
+            }
+            games_list.append( game_dict )
+        
+        response = make_response( games_list, 200  )
+        
+        return response
+
+    elif request.method == 'POST':
+        game_info = request.get_json()
+        game = Game( 
+            title = game_info['title'], price = game_info[ 'price' ] 
+        )
+        db.session.add( game )
+        db.session.commit()
+        return make_response( game.to_dict(), 201 )
 
 
 
-@app.route( '/games/<int:id>' )
+@app.route( '/games/<int:id>', methods = [ 'GET', 'PATCH', 'DELETE' ] )
 def games_by_id( id ):
     
     game = Game.query.filter( Game.id == id ).first()
 
-    response = make_response( game.to_dict(), 200  )
-    
-    return response
+    if game == None:
+        return make_response( { "error": "Game not found" }, 404 )
+
+    if request.method == 'GET':
+        response = make_response( game.to_dict(), 200  )
+        return response
+    elif request.method == 'PATCH':
+        # edit the above "game" instance, add & commit
+    elif request.method == 'DELETE'
+        #delete from database 
 
 
 
--- server/models.py
+++ server/models.py
@@ -4,33 +4,40 @@ from sqlalchemy_serializer import SerializerMixin
 
 db = SQLAlchemy()
 
+
+
 class Game( db.Model, SerializerMixin ):
     __tablename__ = 'games'
-
     serialize_rules = ( '-reviews.game', )
 
     id = db.Column( db.Integer, primary_key = True )
     title = db.Column( db.String )
     price = db.Column( db.Float )
+
     reviews = db.relationship( 'Review', backref='game' )
 
+
+
 class Review( db.Model, SerializerMixin ):
-
-    serialize_rules = ( '-game.reviews', '-user.reviews', '-game_id', '-user_id' )
-
     __tablename__ = 'reviews'
+    serialize_rules = ( '-game.reviews', '-user.reviews', '-game_id', 
+                        '-user_id' )
+
     id = db.Column( db.Integer, primary_key = True )
     content = db.Column( db.String )
+
     user_id = db.Column( db.Integer, db.ForeignKey( 'users.id' ) )
     game_id = db.Column( db.Integer, db.ForeignKey( 'games.id' ) )
 
+
+
 class User( db.Model, SerializerMixin ):
-
-    serialize_rules = ( '-reviews.user', )
-
     __tablename__ = 'users'
+    serialize_rules = ( '-reviews.user', )
+    
     id = db.Column( db.Integer, primary_key = True )
     name = db.Column( db.String )
+    
     reviews = db.relationship( 'Review', backref='user' )
 
    
